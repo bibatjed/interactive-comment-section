b2e832026c5f12900a1b3d8d4acd8c9ae1927671
Jeeed <43069218+bibatjed@users.noreply.github.com>
2022-05-22 19:02:55 +0800

    feat: rating component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,17 +1,19 @@
 import iconPlusSrc from "../../images/icon-plus.svg";
 import iconMinusSrc from "../../images/icon-minus.svg";
 
+import "./_style.scss";
+
 type RatingProps = {
   score: number;
 };
 export default function Rating(props: RatingProps) {
   return (
     <div className="rating">
-      <button className="rating_up_btn">
+      <button className="rating__up__btn">
         <img src={iconPlusSrc} alt="" />
       </button>
       <span className="rating__score"> {props.score} </span>
-      <button className="rating_down_btn">
+      <button className="rating__down__btn">
         <img src={iconMinusSrc} alt="" />
       </button>
     </div>
