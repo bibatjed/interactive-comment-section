635345914e865bfdb5e3ea6a3676bf236362908b
Jeeed <43069218+bibatjed@users.noreply.github.com>
2022-05-22 20:15:54 +0800

    feat: add for reply

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -21,44 +21,50 @@ type CommentProps = {
   createdAt: string;
   score: number;
   user: User;
+  isReply?: boolean;
   replies?: Replies[];
 };
 
 export default function Comment(props: CommentProps) {
   return (
-    <div className="comment">
-      <div className="comment__rating">
-        <Rating score={props.score} />
-      </div>
-      <div className="comment__content">
-        <div className="comment__content__header">
-          <div className="comment__content__header__user">
-            <div className="comment__content__header__user__image">
-              <img
-                src={profileImage(props.user.username)}
-                alt={props.user.username}
-              />
+    <>
+      <div className={`comment ${props.isReply ? "reply" : ""}`}>
+        <div className="comment__rating">
+          <Rating score={props.score} />
+        </div>
+        <div className="comment__content">
+          <div className="comment__content__header">
+            <div className="comment__content__header__user">
+              <div className="comment__content__header__user__image">
+                <img
+                  src={profileImage(props.user.username)}
+                  alt={props.user.username}
+                />
+              </div>
+              <span className="comment__content__header__user__name">
+                {props.user.username}
+              </span>
             </div>
-            <span className="comment__content__header__user__name">
-              {props.user.username}
+            <span className="comment__content__header__time">
+              {props.createdAt}
             </span>
-          </div>
-          <span className="comment__content__header__time">
-            {props.createdAt}
-          </span>
 
-          <div className="comment__content__header__reply">
-            <button>
-              <img src={replySrc} alt="replybtn" />
-              <span>Reply</span>
-            </button>
+            <div className="comment__content__header__reply">
+              <button>
+                <img src={replySrc} alt="replybtn" />
+                <span>Reply</span>
+              </button>
+            </div>
           </div>
-        </div>
 
-        <div className="comment__content__message">
-          <p>{props.content}</p>
+          <div className="comment__content__message">
+            <p>{props.content}</p>
+          </div>
         </div>
       </div>
-    </div>
+      {props.replies?.map((value) => {
+        return <Comment key={value.id} isReply={true} {...value} />;
+      })}
+    </>
   );
 }
