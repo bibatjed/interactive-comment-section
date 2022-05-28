a7a4676f5eb43541cf7b0b46f7c35154ada3d3ef
Jeeed <43069218+bibatjed@users.noreply.github.com>
2022-05-28 15:49:08 +0800

    feat: add style for comment input

--- src/components/CommentInput/index.tsx
+++ src/components/CommentInput/index.tsx
@@ -1,4 +1,6 @@
 import { ChangeEvent, FormEvent, useState } from "react";
+import profileImage from "../../utilities/ProfileImage";
+import "./_style.scss";
 
 interface CommentInputPropsMethod {
   onSubmit: (value: string) => void;
@@ -6,7 +8,7 @@ interface CommentInputPropsMethod {
 
 export default function CommentInput(props: CommentInputPropsMethod) {
   const [comment, setComment] = useState("");
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setComment(e.target.value);
   };
 
@@ -18,9 +20,16 @@ export default function CommentInput(props: CommentInputPropsMethod) {
     }
   };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChangeHandler} value={comment}></input>
+    <div className="comment__input">
+      <div className="comment__input__img">
+        <img src={profileImage("juliusomo")} />
+      </div>
+      <form onSubmit={onSubmit} className="comment__input__form">
+        <textarea
+          placeholder="Add a comment..."
+          onChange={onChangeHandler}
+          value={comment}
+        ></textarea>
         <button type="submit">Send</button>
       </form>
     </div>
